dbf732985181fc098929f600bc8b77d08fe7de84
Mayvis <kevin73911@gmail.com>
2021-11-14 11:47:04 +0800

    git init

---
+++ src/api/api.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+const app = express();
+
+// middle
+app.use(cors())
+app.use(express.json());
+
+const swaggerDefinition = {
+  swagger: "2.0",
+  info: {
+    title: "Monoame API serverless swagger jsdoc template",
+    description: "Monoame API serverless swagger jsdoc template documentation",
+    version: "1.0.0",
+  },
+}
+const options = {
+  swaggerDefinition,
+  apis: ["./src/api/routes/*.js"]
+}
+const swaggerDoc = swaggerJSDoc(options);
+
+app.use("/swagger",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDoc, {
+    swaggerOptions: {
+      url: 'api-docs'
+    }
+  })
+);
+
+// routes
+app.use("/", routes);
+
+module.exports = app;
---
+++ src/api/routes/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+/**
+ * @swagger
+ * /api/v1/test:
+ *   get:
+ *     tags:
+ *       - test
+ *     name: Test
+ *     summary: Test API
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       '200':
+ *         description: Connection has been established successfully.
+ */
+router.get("/api/v1/test", async (req, res) => {
+  res.status(200).json({message: "ok"});
+});
+
+module.exports = router;
